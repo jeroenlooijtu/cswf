019d828ad664c9356c5d6a2014238a9bcf595905
jeroenlooijtu <jeroen.looij2406@gmail.com>
2023-11-17 08:39:31 +0100

    Started angular setup, commit to continue on lapto

--- libs/cswf-project/features/src/lib/features.module.ts
+++ libs/cswf-project/features/src/lib/features.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MealListComponent } from './meal/meal-list/meal-list.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [MealListComponent],
 })
 export class FeaturesModule {}
---
+++ libs/cswf-project/features/src/lib/meal/meal-list/meal-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cswf-project-meal-list',
+  templateUrl: './meal-list.component.html',
+  styleUrls: ['./meal-list.component.css'],
+})
+export class MealListComponent {}
