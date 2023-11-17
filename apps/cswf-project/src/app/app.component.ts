073f5e1e56914bf97dc5c6055e54b4349ee9eab6
Jeroen Looij <J.looij@student.tudelft.nl>
2023-11-17 11:46:46 +0100

    Finished all the basics When i come back you'll still be here

--- apps/cswf-project/src/app/app.component.ts
+++ apps/cswf-project/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { FeaturesModule } from '@cswf-project/features';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, FeaturesModule],
   selector: 'cswf-project-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
---
+++ libs/cswf-project/features/src/lib/meal/meal-detail/meal-detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cswf-project-meal-detail',
+  template: `<p>meal-detail works!</p>`,
+  styles: [],
+})
+export class MealDetailComponent {}
