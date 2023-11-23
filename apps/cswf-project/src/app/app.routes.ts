c7a5c5ab57e079140371258d8fbf7f6f7811917d
Jeroen Looij <J.looij@student.tudelft.nl>
2023-11-23 14:08:21 +0100

    started creating about page

--- apps/cswf-project/src/app/app.routes.ts
+++ apps/cswf-project/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Route } from '@angular/router';
+import { AboutPageComponent } from 'libs/cswf-project/features/src/lib/about/about-page/about-page.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {path: 'about', component: AboutPageComponent}
+];
--- libs/cswf-project/features/src/lib/features.module.ts
+++ libs/cswf-project/features/src/lib/features.module.ts
@@ -4,11 +4,12 @@ import { MealListComponent } from './meal/meal-list/meal-list.component';
 import { MealDetailComponent } from './meal/meal-detail/meal-detail.component';
 import { MealService } from './meal/meal.service';
 import { HttpClientModule } from '@angular/common/http';
+import { AboutPageComponent } from './about/about-page/about-page.component';
 
 @NgModule({
   imports: [CommonModule, HttpClientModule],
-  declarations: [MealListComponent, MealDetailComponent],
+  declarations: [MealListComponent, MealDetailComponent, AboutPageComponent],
   providers: [MealService],
-  exports: [MealListComponent, MealDetailComponent]
+  exports: [MealListComponent, MealDetailComponent],
 })
 export class FeaturesModule {}
